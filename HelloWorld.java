bb0186ea27c42904c00fc373531d1f43f0d5d155
roshantodi09 <roshantodi40@gmail.com>
2026-06-16 02:03:21 +0530

    imported scanner class from java utility and took console input of var a and printed it

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
     }
 }
