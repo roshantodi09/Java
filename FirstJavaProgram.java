51081a361c0a4d59a9d4825d5a7f131147a1efd5
roshantodi09 <roshantodi40@gmail.com>
2026-06-16 03:11:30 +0530

    comment added for readability

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class HelloWorld{
+public class FirstJavaProgram{
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
 
+        //input
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a);
